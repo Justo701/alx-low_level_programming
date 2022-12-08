2ebf5a4253c87e992d6abb67526ef21c3b4fb420
Korkou Justus <korkoujustus@gmail.com>
2022-12-08 10:44:23 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+
+
+
+}
