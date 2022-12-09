3eb0ef9e540a80def3ef6a9d8b8433391b212bd0
Korkou Justus <korkoujustus@gmail.com>
2022-12-09 06:28:37 -0800

    size 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
 printf("Size of a char: %lu byte(s)\n",sizeof(char));
 printf("Size of an int: %lu byte(s)\n",sizeof(int));
@@ -13,6 +15,7 @@ printf("Size of a long int: %lu byte(s)\n",sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n",sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n",sizeof(float));
 return (0);
+
 }
 
 
