5139198bd620206e7b6141b59fe02bc341915ad0
Korkou Justus <korkoujustus@gmail.com>
2022-12-09 01:56:27 -0800

    size6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
-
- Return 0 (Always)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 printf("Size of a char: %lu(bytes)\n",sizeof(char));
@@ -15,5 +14,6 @@ printf("Size of a long long int: %lu(bytes)\n",sizeof(long long int));
 printf("Size of a float: %lu(bytes)\n",sizeof(float));
 return (0);
 }
+
 
 
