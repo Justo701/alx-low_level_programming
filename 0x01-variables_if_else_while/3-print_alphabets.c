ef8951f0dd3366d48e0a495d123859e93b7d61ec
Korkou Justus <korkoujustus@gmail.com>
2022-12-12 08:53:37 -0800

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	int CH = 'A';
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
