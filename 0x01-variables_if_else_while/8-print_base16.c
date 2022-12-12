c6193beb5bed0393f8edd4ed207674f39c05351d
Korkou Justus <korkoujustus@gmail.com>
2022-12-12 12:07:56 -0800

    Hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+
+	d = 0;
+
+	while (d < 10)
+	{
+		putchar (d + '0');
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
