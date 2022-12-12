07b099daa71d1ee5b4f8caa2f9551d24f2e5dab5
Korkou Justus <korkoujustus@gmail.com>
2022-12-12 07:46:38 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+	int n;
+	(m = 'a', n = 'z');
+	for (m < n;
+		putchar(m++);
+	putchar('\n');
+	return (0);
+}
+
