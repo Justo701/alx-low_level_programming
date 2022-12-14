0bedd353825ec80d763d7fdb063c0b91db0fe1fa
Korkou Justus <korkoujustus@gmail.com>
2022-12-14 10:06:03 -0800

    lowercase checking

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * @c: is the character to be checked
  * Return: 1 if char is lowercase, otherwise return 0
  */
-int _is lowercase(int c)
+int _islower(int c)
 {
 	if (c > 'a' && c <= 'z')
 		return (1);
