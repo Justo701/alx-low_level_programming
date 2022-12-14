0fec6cc181d74cbfb209ce7cef04a6bcba360647
Korkou Justus <korkoujustus@gmail.com>
2022-12-14 09:02:58 -0800

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - all alphabet in lowecase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+	return (0);
+}
