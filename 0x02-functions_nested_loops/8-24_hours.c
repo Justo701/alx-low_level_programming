a26c4bac892ae047f2435ac39d3d109772fbab8b
Korkou Justus <korkoujustus@gmail.com>
2022-12-15 01:37:13 -0800

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,8 +19,8 @@ void jack_bauer(void)
 
 					{
 						_putchar(a + '0');
-						_putchar(b + '0);
-						_putchar(58);
+						_putchar(b + '0');
+						_putchar('58');
 						_putchat(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
