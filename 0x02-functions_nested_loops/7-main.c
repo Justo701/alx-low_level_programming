3792a6c0476478fdaf681f82d8df740d604a72e3
Korkou Justus <korkoujustus@gmail.com>
2022-12-15 00:53:12 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,16 @@
+#imclude "main.c"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+	print_last _digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
+
