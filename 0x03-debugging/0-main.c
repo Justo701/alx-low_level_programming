35e09224029f8326d9a90d20313d7b09ddf9b898
Korkou Justus <korkoujustus@gmail.com>
2022-12-15 10:16:14 -0800

    positives or negatives

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -9,5 +10,7 @@ int main(void)
 
 	i = 98;
 	positive_or_negative(i);
-return (0);
-}}
+
+	return (0);
+}
+
