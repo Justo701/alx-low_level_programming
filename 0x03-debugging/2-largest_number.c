804c09e790135da9451cb4900a4d4eb20bd42008
Korkou Justus <korkoujustus@gmail.com>
2022-12-15 12:03:00 -0800

    largest

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * largest_number - returvs the the largest of 3 numbers
+ * @a: first number
+ * @b: second number
+ * @c: third number
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest number;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
