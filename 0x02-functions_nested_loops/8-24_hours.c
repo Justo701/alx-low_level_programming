d054e563f75a27e71e071860b5c4ac820e4c13e7
Korkou Justus <korkoujustus@gmail.com>
2022-12-15 04:52:32 -0800

    every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,21 @@
+
 #include "main.h"
 /**
- * jack_bauer - print every minute of tge day of Jack Buer
- * Returns: ...
+ * jack_bauer - print every minute of day
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int hour, minutes;
 
-	for (a = 0; b <= 2; a++)
+	for (hour = 0, hour <= 23; hour++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (minutes = 0; minutes <= 59; minutes++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
-
