df00a5750a133a079938a416480e1a5982735a4d
Korkou Justus <korkoujustus@gmail.com>
2022-12-16 03:51:10 -0800

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,16 @@
 
 /**
  * print_numbers - print num from 0-9
- *
  * return: numbers 0 to 9
  */
 
 void print_numbers(void)
 {
-	int c;
+	int x;
 
-	for (c = 0; c <= 9; c++)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar(c + '0');
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
