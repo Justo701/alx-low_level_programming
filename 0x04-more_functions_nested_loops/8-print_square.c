835bb025fee8d049b6156119d9e2f52e1f087237
Korkou Justus <korkoujustus@gmail.com>
2022-12-16 13:16:49 -0800

    a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: the number of square
+ * Return: empty
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
+
