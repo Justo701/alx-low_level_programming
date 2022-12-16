68100208d7c8fdc481673cd3caca5cd39afb142b
Korkou Justus <korkoujustus@gmail.com>
2022-12-16 11:40:17 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,17 +10,15 @@ void more_numbers(void)
 
 	for (x = 0; x <= 10; x++)
 	{
-		for (y = 0; y <= 14; y++)
-
-		{
-			if (y > 9)
-
-			{
-			_putchar((y / 10) + '0');
-			}
-			_putchar((y % 10) + '0');
-			}
-		_putchar('\n');
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
 	}
 }
 
