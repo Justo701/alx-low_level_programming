3f82585d1a8cfddb4bde7d94fee5cc9dd7558c44
Korkou Justus <korkoujustus@gmail.com>
2022-12-16 02:12:53 -0800

    check uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#nclude "main.h"
+
+/**
+ * _isupper - checks uppercase character
+ * @c: character to be checked
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
