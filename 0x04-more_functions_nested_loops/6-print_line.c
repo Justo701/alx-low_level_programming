cb0f2f98268e7cf582dae8a97a7164886c548700
Korkou Justus <korkoujustus@gmail.com>
2022-12-16 11:23:55 -0800

    draw straight lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,6 +12,7 @@ void print_line(int n)
 	if (n <= 0)
 	{
 	_putchar('\n');
+	}
 	else
 	{
 	for (x = 0; x < n; x++)
@@ -19,6 +20,5 @@ void print_line(int n)
 	_putchar(95);
 	}
 	_putchar('\n');
-	}
 	}
 }
