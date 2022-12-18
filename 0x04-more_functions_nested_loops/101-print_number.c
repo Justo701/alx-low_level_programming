4e3845627cc3a03ea91e769d5f7c34e3359c1647
Korkou Justus <korkoujustus@gmail.com>
2022-12-18 00:51:07 -0800

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,16 +9,21 @@
 
 void print_number(int n)
 {
-	unsigned int k = n;
+	unsigned int k;
 
 	if (n < 0)
 	{
-	n = -1;
-	k = n;
+	k = -n;
 	_putchar('-');
 	}
-	k /= 10;
-	if (k != 0)
-	print_number(k);
-	_putchar((unsigned int) n % 10 + '0');
+	else
+	{
+	k = n;
+	}
+	if (k / 10)
+	{
+	print_number(k / 10);
+	}
+	_putchar((k % 10) + '0');
 }
+
