f15210e4c33369ef30e629c2d977cd226cd2b344
Korkou Justus <korkoujustus@gmail.com>
2022-12-21 23:52:44 -0800

    copy n characters to a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n chars from src to dest
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of chars to be copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+	j++;
+	dest[j] = src[j];
+	}
+	while (j < n)
+	{
+	dest[j] = '\0';
+	j++;
+	}
+	return (dest);
+}
+
