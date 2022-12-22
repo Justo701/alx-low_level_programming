68c4c2acf19b90f809522bd9fbc337df65258db0
Korkou Justus <korkoujustus@gmail.com>
2022-12-22 00:10:46 -0800

    task 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-        j = 0;
+	j = 0;
 	while (j < n && src[j] != '\0')
 	{
 	dest[j] = src[j];
@@ -22,6 +22,7 @@ char *_strncpy(char *dest, char *src, int n)
 	dest[j] = '\0';
 	j++;
 	}
+
 	return (dest);
 }
 
