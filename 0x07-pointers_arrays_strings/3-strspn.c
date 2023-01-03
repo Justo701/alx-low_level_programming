1edaf5311c8e28b38837853be7061dbe42165348
Korkou Justus <korkoujustus@gmail.com>
2023-01-03 10:50:25 -0800

    strpsn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,20 +8,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+	unsigned int n = 0;
+	int r;
 
-	value = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-	check = 0;
-	for (n = 0; accept[n] != '\0'; n++)
+	for (r = 0; accept[r]; r++)
 	{
-	if (accept[n] == s[i])
+	if (*s == accept[r])
 	{
-	value++;
-	check = 1;
+	n++;
+	break;
 	}
+	else if (accept[r + 1] == '\0')
+	return (n);
 	}
+	s++;
 	}
+	return (n);
 }
 
