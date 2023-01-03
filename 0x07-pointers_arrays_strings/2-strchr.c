a0eddc13edd35f54e99eec6e484f80c7ae8008ba
Korkou Justus <korkoujustus@gmail.com>
2023-01-03 07:55:54 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string.
+ * @c: input
+ * @s: input
+ * Return: Always 0 on Success
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+	if (s[i] == c)
+	return (s + i);
+	}
+	return (NULL);
+}
