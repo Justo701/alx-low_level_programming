83a8dbc6eb61193c982759c7dc4507bab49c3788
Korkou Justus <korkoujustus@gmail.com>
2023-01-10 07:13:30 -0800

    0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
-#include "main.h"
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - prints its name
- * @argc: argument count
- * @argv: argument vector
- * Return: Always 0
+ * main - prints the name of the program
+ * @argc: array size
+ * @argv: array containing elements
+ *
+ *Return: 0 on Success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	puts(argv[argc - 1]);
 	return (0);
 }
