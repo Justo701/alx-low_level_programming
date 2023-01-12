b8b19fddecf99e6a67dd464c11125fe4126f9889
Korkou Justus <korkoujustus@gmail.com>
2023-01-12 10:11:26 -0800

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 	return (NULL);
-	mee = malloc(sizeof(int*) * height);
+	mee = malloc(sizeof(int *) * height);
 	if (mee == NULL)
 		return (NULL);
 	for (x = 0; x < height; x++)
