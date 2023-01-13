8baa0ff0e9a71ad20f8a33b38b49d421fc993380
Korkou Justus <korkoujustus@gmail.com>
2023-01-13 11:58:11 -0800

    create-array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	ar = malloc(sizeof(*ar) * ((max - min) + 1));
 	if (ar == NULL)
 	return (NULL);
-	for (i = 0; min < max; i++, min++)
+	for (i = 0; min <= max; i++, min++)
 	ar[i] = min;
 	return (ar);
 }
