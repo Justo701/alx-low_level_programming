0945881e3bde01b54824e392da0a27f1feafd63f
Korkou Justus <korkoujustus@gmail.com>
2023-01-13 11:14:12 -0800

    task_1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,14 +11,16 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *sout;
-	unsigned int ls1, ls1, ls2, lsout, i;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
 	s1 = " ";
 	if (s2 == NULL)
 	s2 = " ";
 	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+	;
 	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+	;
 	if (n > ls2)
 	n = ls2;
 	lsout = ls1 + n;
