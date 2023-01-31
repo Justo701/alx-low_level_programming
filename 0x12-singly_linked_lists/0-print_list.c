5037b2c6b970a179284ce4dd1f7f926eb7695280
Korkou Justus <korkoujustus@gmail.com>
2023-01-31 07:19:23 -0800

    print elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 	if (h->str == NULL)
-	printf("[%d]%s\n", 0, "(nil)");
+	printf("[%d] %s\n", 0, "(nil)");
 	else
-	printf("[%d]%s\n", h->len, h->str);
+	printf("[%d] %s\n", h->len, h->str);
 	h = h->next;
 	nelem++;
 	}
