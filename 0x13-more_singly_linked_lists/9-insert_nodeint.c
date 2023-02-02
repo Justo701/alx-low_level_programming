210ff604b9634d5d5271fffb07b0a4e731667bdb
Korkou Justus <korkoujustus@gmail.com>
2023-02-02 12:40:45 -0800

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,8 +16,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (!new || !head)
 	return (NULL);
+
 	new->n = n;
 	new->next = NULL;
+
 	if (idx == 0)
 	{
 	new->next = *head;
