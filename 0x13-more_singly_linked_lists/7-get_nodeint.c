35c466004e35d58f093748d9b44ecf7323fd1c80
Korkou Justus <korkoujustus@gmail.com>
2023-02-02 11:23:20 -0800

    7-get_nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *temp = head;
 
-	while (*temp && i < index)
+	while (temp && i < index)
 	{
 	temp = temp->next;
 	i++;
 	}
 	return (temp? temp: NULL);
 }
+
+
