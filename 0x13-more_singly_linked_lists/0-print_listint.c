3a2dab1b108194da12f8ec5f49f4217d80f1e6cb
Korkou Justus <korkoujustus@gmail.com>
2023-02-02 06:15:32 -0800

    print elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all elements of a list
+ * @h: list of type listint_t to print
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h);
+{
+	size_int num = 0;
+
+	while (h)
+	{
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+	}
+	return (num);
+}
