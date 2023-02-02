039f12071be5e5e5109377be325bb4248333606f
Korkou Justus <korkoujustus@gmail.com>
2023-02-02 12:57:58 -0800

    10-delete_nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
-	listint_t *current = NuLL;
+	listint_t *current = NULL;
 	unsigned int i = 0;
 
 	if (*head == NULL)
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	temp = temp->next;
 	i++;
 	}
-	currrent = temp->next;
+	current = temp->next;
 	temp->next = current->next;
 	free(current);
 	return (1);
